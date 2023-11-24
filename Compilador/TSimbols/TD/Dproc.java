30ecaf8207dc59f3d2ca10024380581864c933b5
enVives <perejoanvm@gmail.com>
2023-11-25 00:26:18 +0100

    canvis sintaxis
    canvis de declaracions i constants que
    afavoreixen sa simplicitat des semantic

--- Compilador/TSimbols/TD/Dproc.java
+++ Compilador/TSimbols/TD/Dproc.java
@@ -2,10 +2,23 @@ package Compilador.TSimbols.TD;
 
 public class Dproc extends Descripcio {
     private static Integer np =0;
+    private String tipus;
+
+
+    public String getTipus() {
+        return tipus;
+    }
 
     public Dproc(){
         super("dproc");
         np++;
+        tipus = "id_nula";
+    }
+
+     public Dproc(String tipus){
+        super("dproc");
+        np++;
+        this.tipus = tipus;
     }
 
     public Integer np(){
