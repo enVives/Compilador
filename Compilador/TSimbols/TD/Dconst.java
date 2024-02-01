7adf6ba42a95edbae09a5ed78c675c2cd89d4a01
enVives <perejoanvm@gmail.com>
2024-02-01 23:57:08 +0100

    darrers ajustos

--- Compilador/TSimbols/TD/Dconst.java
+++ Compilador/TSimbols/TD/Dconst.java
@@ -1,26 +1,33 @@
 package Compilador.TSimbols.TD;
+
 public class Dconst extends Descripcio {
-    
+
     private Object valor;
     private String tipus;
+    private Integer temporal;
 
-    public Dconst(Object v, String t){
+    public Integer getTemporal() {
+        return temporal;
+    }
+
+    public Dconst(Object v, String t) {
         super("dconst");
         tipus = t;
         valor = v;
     }
 
-    public Dconst(String t){
+    public Dconst(String t, Integer temporal) {
         super("dconst");
         tipus = t;
+        this.temporal = temporal;
         valor = null;
     }
 
-    public Object valor(){
+    public Object valor() {
         return valor;
     }
 
-    public String tipus(){
+    public String tipus() {
         return tipus;
     }
 }
--- Principal/Principal.java
+++ Principal/Principal.java
@@ -13,26 +13,25 @@ import java_cup.runtime.SymbolFactory;
 
 import Compilador.Lexic.Scanner;
 import Compilador.Sintactic.Parser;
+
 public class Principal {
     /**
      * @param args arguments de línia de comanda
      */
     public static void main(String[] args) {
         Reader fitxer;
-        boolean bol = 1+1<=1+1;
-        Double b = 1 % 1 * 1.25;
         try {
-            fitxer = new FileReader("/home/perejoan/Documentos/GitHub/Compilador/Principal/prova.txt");
+            fitxer = new FileReader("c:\\Users\\perej\\Documents\\GitHub\\Compilador\\Principal\\prova.txt");
             Scanner scanner = new Scanner(fitxer);
-        
+
             SymbolFactory sf = new ComplexSymbolFactory();
             Parser parser = new Parser(scanner, sf);
             parser.parse();
             parser.mostra_intermedi();
-        } catch(Exception e) {
-            System.err.println("error: "+e);
+        } catch (Exception e) {
+            System.err.println("error: " + e);
             e.printStackTrace(System.err);
         }
     }
-    
+
 }
